3a4aa23d2213dcfaeaed964b5d09d89af72109f4
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-12-01 06:35:51 -0800

    Search form working, just need to fix fetch request to return search results

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
-  state = {};
+  state = {
+    jobTitle: "",
+    location: ""
+  };
+
+  handleInput = event => {
+    event.persist();
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.persist();
+    event.preventDefault();
+    // fetch search results using current search state
+  };
+
   render() {
     return (
       <div>
-        <input type="text" placeholder="job title" />
-        <input type="text" placeholder="location" />
-        <button>Search</button>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleInput}
+            type="text"
+            placeholder="job title"
+            name="jobTitle"
+            value={this.state.jobTitle}
+          />
+          <input
+            onChange={this.handleInput}
+            type="text"
+            placeholder="location"
+            name="location"
+            value={this.state.location}
+          />
+          <button>Search</button>
+        </form>
       </div>
     );
   }
