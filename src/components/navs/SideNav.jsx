63fcca6adaab54b2cd9d0cb7af1c69c7cea57a3c
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-12-01 06:22:30 -0800

    Add search bar with no styling

--- src/components/navs/SideNav.jsx
+++ src/components/navs/SideNav.jsx
@@ -5,13 +5,6 @@ class SideNav extends Component {
   state = {};
   render() {
     return (
-      // <div className="sidenav-container">
-      //   <div className="sidenav-links-container">
-      //     <div className="sidenav-link">Search</div>
-      //     <div className="sidenav-link">Board</div>
-      //     <div className="sidenav-link">Profile</div>
-      //   </div>
-      // </div>
       <div>
         <nav>
           <Link to="search">Search</Link>
--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -3,7 +3,13 @@ import React, { Component } from "react";
 class SearchBar extends Component {
   state = {};
   render() {
-    return <div>Search Bar</div>;
+    return (
+      <div>
+        <input type="text" placeholder="job title" />
+        <input type="text" placeholder="location" />
+        <button>Search</button>
+      </div>
+    );
   }
 }
 
