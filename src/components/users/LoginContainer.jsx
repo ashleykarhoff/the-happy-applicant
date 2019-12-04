f7e3bc51971f61565ae485895abf23660d515fa1
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-12-03 20:13:18 -0500

    Install SVGR, converted svg icons into react components and restyled sidebar nav with icons

--- src/components/users/LoginContainer.jsx
+++ src/components/users/LoginContainer.jsx
@@ -1,22 +1,66 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin-left: 200px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.h2`
+  font-size: 32px;
+`;
+
+const Text = styled.div`
+  font-size: 18px;
+  max-width: 650px;
+`;
+
+const SubHeader = styled.h4`
+  font-size: 24px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 450px;
+`;
+
+const Input = styled.input`
+  border: solid 0.5px lightgrey;
+  border-radius: 20px;
+  padding: 8px;
+  margin: 8px;
+  font-size: 16px;
+`;
+
+const Button = styled.button`
+  border: solid 0.5px lightgrey;
+  border-radius: 20px;
+  padding: 8px;
+  margin: 8px;
+  font-size: 16px;
+  background-color: #03176a;
+  color: white;
+`;
 
 class LoginContainer extends Component {
   state = {};
   render() {
     return (
-      <div className="login-container">
-        <h2>Welcome back!</h2>
-        <p>
+      <Container>
+        <Header>Welcome back!</Header>
+        <Text>
           Gone are the dark days of Excel spreadsheets and copy and pasting to
           organize your job search. Join the Happy Applicant community for
           access.
-        </p>
-        <h5>Sign into your account!</h5>
-        <form action="">
-          <input type="text" placeholder="Email address" />
-          <input type="text" placeholder="Password" />
-          <button>Login</button>
-        </form>
+        </Text>
+        <SubHeader>Sign into your account!</SubHeader>
+        <Form action="">
+          <Input type="text" placeholder="Email address" />
+          <Input type="text" placeholder="Password" />
+          <Button>Login</Button>
+        </Form>
         <div>
           <p>
             Need to create an account? <span>Sign up</span>
@@ -24,7 +68,7 @@ class LoginContainer extends Component {
           <p>Forgot password?</p>
           <p>Terms and Conditions</p>
         </div>
-      </div>
+      </Container>
     );
   }
 }
